6d6bdead9d350e47733f4af6b8a0bdfc4edaf5b3
ollie8 <ollie8@localhost>
2013-05-29 20:10:46 +0000

    docs;

--- Menu.h
+++ Menu.h
@@ -18,39 +18,39 @@ private:
 public:
     typedef void (*SelectionChange)(MenuItem*);
     
-    /** Constructs a new MenuItem object. The constructor takes two function pointers as
+    /* Constructs a new MenuItem object. The constructor takes two function pointers as
      * arguments both of which must take a single argument of a MenuItem pointer.
      *
      * Example:
-     * @code
+     * @code example
      *  MenuItem *item = new MenuItem(&enterItem, &select, name);
      * @end code
      *
-     * @param EnterMenuItem - A pointer to a callback fuction called when the item is entered.
-     * @param SelectionChange - A pointer to a callback fuction called when the item is selected.
-     * @param name - The name of the MenuItem.
+     * @param EnterMenuItem A pointer to a callback fuction called when the item is entered.
+     * @param SelectionChange A pointer to a callback fuction called when the item is selected.
+     * @param name The name of the MenuItem.
      */
     MenuItem(EnterMenuItem, SelectionChange, char*);
     
-    /** Gets the name of the item.
+    /* Gets the name of the item.
      * @return A char array containing the item name.
      */
     char* getName();
     
-    /** Enters this instance of MenuItem and ultimatley calls the entered callback function.
+    /* Enters this instance of MenuItem and ultimatley calls the entered callback function.
      */
     virtual void enter();
     
-    /** Determines wheter tgis instance is selected.
+    /* Determines wheter tgis instance is selected.
      * @return true if selected.
      */
     bool isSelected();
     
-    /** Selects this instance of MenuItem in its containing Menu and called the selected call back function.
+    /* Selects this instance of MenuItem in its containing Menu and called the selected call back function.
      */
     void select();
     
-    /** De-selectes this instance - this is called internally by Menu DO NOT CALL THIS.
+    /* De-selectes this instance - this is called internally by Menu DO NOT CALL THIS.
      */
     void deSelect();
 protected:
@@ -59,7 +59,7 @@ protected:
     MenuItem();
 };
 
-/** Menu represents a container for a set of MenuItems. Menu is itself
+/* Menu represents a container for a set of MenuItems. Menu is itself
  * a menu item meaning that Menu objects can be used as sun menus. 
  */
 class Menu : public MenuItem {
